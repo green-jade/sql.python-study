e7f6f0df095bc83d0ba630121b940c2060c54a25
Jiwon Kim <77044015+green-jade@users.noreply.github.com>
2024-05-19 12:45:44 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+deq = deque()
+N = int(input())
+
+for i in range(N):
+    cmd = input().split()
+    if cmd[0] == "push_front":
+        deq.appendleft(int(cmd[1]))
+    elif cmd[0] == "push_back":
+        deq.append(int(cmd[1]))
+      
+    elif cmd[0] == "pop_front":
+        try:
+            print(deq.popleft())
+        except IndexError:
+            print(-1)
+    elif cmd[0] == "pop_back":
+        try:
+            print(deq.pop())
+        except IndexError:
+            print(-1)
+
+    elif cmd[0] == "size":
+        print(len(deq))
+    elif cmd[0] == "empty":
+        print(int(len(deq)==0))
+
+    elif cmd[0] == "front":
+        try:
+            print(deq[0])
+        except IndexError:
+            print(-1)
+    else:
+        try:
+            print(deq[-1])
+        except IndexError:
+            print(-1)
